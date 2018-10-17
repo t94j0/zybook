63ae742103fdaff0d297c04b088d17e5626ecfe3
Max Harley <augeocacher@gmail.com>
2018-10-16 20:04:51 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='zybook',
+      version='1.0',
+      description='Solves ZyBooks Answers',
+      author='Max Harley',
+      author_email='maxh@maxh.io',
+      url='http://github.com/t94j0/zybook',
+     )
